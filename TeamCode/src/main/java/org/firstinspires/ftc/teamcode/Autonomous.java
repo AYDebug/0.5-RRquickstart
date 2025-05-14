b08afab57fcdb4f264b71d25ac3a39862742c9ec
ArKnee <145818188+AYDebug@users.noreply.github.com>
2025-05-14 15:56:29 -0500

    Update Autonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -22,4 +22,30 @@ public class Autonomous
         
     }
 
+    public void init(HardwareMap ahwMap)
+    {
+        hwMap-ahwMap;
+
+        colorSensor = hwMap.get(ColorSensor.class, "color sensor");
+
+        leftRearDrive = hwMap.get(DcMotor.class, "motor_rear_left");
+        rightRearDrive = hwMap.get(DcMotor.class, "motor_rear_right");
+        leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightRearDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftRearDrive.setPower(0);
+        rightRearDrive.setPower(0);
+
+        LeftRearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightRearDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        spike - hwMap.get(Servo.class, "servo");
+        spike.setPosition(.2);
+        
+    }
+
 }
+
+
